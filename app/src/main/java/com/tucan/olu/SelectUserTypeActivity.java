69fa0cdc7f29f6369de78f3aceffe7886cef37ef
Rohit Garg <rohitgarg@Rohits-Mac-mini.fritz.box>
2019-04-28 12:01:36 +0530

    Latest code

--- app/src/main/java/com/tucan/olu/SelectUserTypeActivity.java
+++ app/src/main/java/com/tucan/olu/SelectUserTypeActivity.java
@@ -1,5 +1,7 @@
 package com.tucan.olu;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -21,15 +23,55 @@ public class SelectUserTypeActivity extends GenricActivity {
 
     @OnClick(R.id.userTypeLayout)
     public void userTypeClick(View v) {
-        Intent intent = new Intent(this, LoginScreenActivity.class);
-        AppCommon.getInstance(this).setCurrentUser(2);
-        startActivity(intent);
+        showUserPopUp();
     }
 
     @OnClick(R.id.trainerTypeLayout)
     public void trainerTypeLayout(View v) {
-        Intent intent = new Intent(this, LoginScreenActivity.class);
-        AppCommon.getInstance(this).setCurrentUser(1);
-        startActivity(intent);
+        showTrainerPopUp();
+    }
+
+    public void showUserPopUp() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage(getString(R.string.user_popup));
+        builder.setNegativeButton(this.getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setPositiveButton(this.getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                Intent intent = new Intent(SelectUserTypeActivity.this, LoginScreenActivity.class);
+                AppCommon.getInstance(SelectUserTypeActivity.this).setCurrentUser(2);
+                startActivity(intent);
+            }
+        });
+        builder.show();
+    }
+
+    public void showTrainerPopUp() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage(getString(R.string.trainer_popup));
+        builder.setNegativeButton(this.getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setPositiveButton(this.getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                Intent intent = new Intent(SelectUserTypeActivity.this, LoginScreenActivity.class);
+                AppCommon.getInstance(SelectUserTypeActivity.this).setCurrentUser(1);
+                startActivity(intent);
+            }
+        });
+        builder.show();
     }
 }
